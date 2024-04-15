e26b31855a88da4a16f9bbd8e5e970d9943bfaa6
Monica <a01707439@itesm.mx>
2024-04-16 01:49:18 +0200

    Dataset V.0.1

---
+++ script.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+def collect_unique_files_by_name(source_folder, destination_folder):
+    os.makedirs(destination_folder, exist_ok=True)
+    seen_names = set()  # Store seen file names to avoid duplicates
+
+    for root, dirs, files in os.walk(source_folder):
+        for filename in files:
+            if filename not in seen_names:
+                seen_names.add(filename)
+                # Copy file to the destination folder
+                shutil.copy(os.path.join(root, filename), os.path.join(destination_folder, filename))
+
+source_directory = 'C:\\Users\\mayal\\PlagiarismDetector\\dataset\\java_pairs'  # Adjust to your source folder path
+destination_directory = 'C:\\Users\\mayal\\PlagiarismDetector\\dataset\\java_unique'  # Adjust to your destination folder path
+
+collect_unique_files_by_name(source_directory, destination_directory)
